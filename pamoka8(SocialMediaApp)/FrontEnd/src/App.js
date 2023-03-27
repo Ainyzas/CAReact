2b490946fba79961da6956bc1543faa88836c8a2
ainas <puidokasainas@gmail.com>
2023-03-28 00:15:25 +0300

    updated registration styling

--- pamoka8(SocialMediaApp)/FrontEnd/src/App.js
+++ pamoka8(SocialMediaApp)/FrontEnd/src/App.js
@@ -6,10 +6,10 @@ import SignOutButton from './components/SignOutButton/SignOutButton';
 function App() {
   return (
     <div>
-      <MainLayout />
-      {/* <SignInLayout /> */}
-      {/* <SignOutButton />
-      {/* <RegistrationLayout /> */}
+      {/* <MainLayout /> */
+      /* <SignInLayout /> */
+      /* <SignOutButton />*/}
+      <RegistrationLayout />
     </div>
   );
 }
--- pamoka8(SocialMediaApp)/FrontEnd/src/components/layouts/RegistrationLayout/RegistrationLayout.jsx
+++ pamoka8(SocialMediaApp)/FrontEnd/src/components/layouts/RegistrationLayout/RegistrationLayout.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { signUpRequest } from '../../../api-calls/auth';
+import style from './RegistrationLayout.module.css';
 
 export default function RegistrationLayout() {
   const [firstName, setFirstName] = useState('');
@@ -40,23 +41,39 @@ export default function RegistrationLayout() {
     setIsRegistrationSuccessful(signupResponse);
   }
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="firstName">First Name</label>
-      <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-      <br />
-      <label htmlFor="lastName">Last Name</label>
-      <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-      <br />
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <br />
-      <label htmlFor="password">Password</label>
-      <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <br />
-      <label htmlFor="dateOfBirth">Date Of Birth</label>
-      <input type="date" id="dateOfBirth" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} />
-      <br />
-      <button type="submit">Register</button>
+    <form className={style.registration} onSubmit={submitHandler}>
+      <legend className={style.title}>PLEASE REGISTER YOUR ACCOUNT</legend>
+      <section className={style.name}>
+        <label className={style.inputContainer}>
+          <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+          <br />
+          First Name
+        </label>
+        <label className={style.inputContainer}>
+          <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+          <br />
+          Last Name
+        </label>
+      </section>
+
+      <label className={style.inputContainer}>
+        <span>Email</span>
+        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+
+      <label className={style.inputContainer}>
+        <span>Password</span>
+        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+
+      <label className={style.inputContainer}>
+        <span>Date Of Birth</span>
+        <input type="date" id="dateOfBirth" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} />
+      </label>
+
+      <button className={style.registerButton} type="submit">
+        Register
+      </button>
       {registrationUpdateHeading()}
     </form>
   );
