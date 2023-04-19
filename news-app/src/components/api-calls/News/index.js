ff2870d3367d6e621ca88bbee47282e898e87c8c
ainas <puidokasainas@gmail.com>
2023-04-19 19:42:20 +0300

    fixes

--- news-app/src/components/api-calls/News/index.js
+++ news-app/src/components/api-calls/News/index.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 import env from 'react-dotenv';
 
 const NEWS_URL = 'https://newsapi.org/v2/top-headlines?country=lt&pageSize=12';
-const { API_KEY } = env;
+const { REACT_APP_API_KEY } = process.env;
 
 export async function fetchNews(category, page) {
   try {
     let FULL_URL;
     if (category === undefined) {
-      FULL_URL = `${NEWS_URL}&page=${page}&apiKey=${API_KEY}`;
+      FULL_URL = `${NEWS_URL}&page=${page}&apiKey=${REACT_APP_API_KEY}`;
     } else {
-      FULL_URL = `${NEWS_URL}&page=${page}&category=${category}&apiKey=${API_KEY}`;
+      FULL_URL = `${NEWS_URL}&page=${page}&category=${category}&apiKey=${REACT_APP_API_KEY}`;
     }
     const res = await axios.get(FULL_URL);
     return res.data;
